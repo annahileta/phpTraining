3e7c7885dd89e650a678cb539bab57d008b40f0f
annahileta <anna.hileta@sigma.software>
2021-03-24 18:45:18 +0200

    working with parametrs in Views

--- components/View.php
+++ components/View.php
@@ -5,6 +5,12 @@ namespace App\Components;
 class View 
 {
     public static function create(string $template, array $parameters) {
+        foreach ($parameters as $key => $value) {
+            $$key = $value;
+        }
+        
+        ob_start();
         include_once ROOT."/views/$template.php";
+        ob_get_clean();
     }
 }
